ba27e3653382bdd7ad90b92232362c3d1153e522
batoan1221 <batoan1221@gmail.com>
2013-11-23 23:35:46 +0700

    Initial Commit

---
+++ SimpleTable/SimpleTableAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimpleTableAppDelegate.h
+//  SimpleTable
+//
+//  Created by admin on 11/23/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleTable/SimpleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTableViewController.h
+//  SimpleTable
+//
+//  Created by admin on 11/23/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableViewController : UIViewController
+
+@end
